30d350320bc878c1e9b220cf962d49c62a8cfc49
tardi <tardieusamuel@gmail.com>
2023-12-05 14:41:24 +0100

    stable version with snake movement ok

--- src/main/java/com/almasb/fxglgames/drop/DropApp.java
+++ src/main/java/com/almasb/fxglgames/drop/DropApp.java
@@ -1,70 +1,88 @@
-/*
- * FXGL - JavaFX Game Library. The MIT License (MIT).
- * Copyright (c) AlmasB (almaslvl@gmail.com).
- * See LICENSE for details.
- */
-
 package com.almasb.fxglgames.drop;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.MouseEventData;
+import com.almasb.fxgl.input.UserAction;
+import javafx.beans.property.Property;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.effect.Light;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
-import static com.almasb.fxgl.dsl.FXGL.*;
-// NOTE: this import above is crucial, it pulls in many useful methods
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * This is an FXGL version of the libGDX simple game tutorial, which can be found
- * here - https://github.com/libgdx/libgdx/wiki/A-simple-game
- *
- * The player can move the bucket left and right to catch water droplets.
- * There are no win/lose conditions.
- *
- * Note: for simplicity's sake all of the code is kept in this file.
- * In addition, most of typical FXGL API is not used to avoid overwhelming
- * FXGL beginners with a lot of new concepts to learn.
- *
- * Although the code is self-explanatory, some may find the comments useful
- * for following the code.
- *
- * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
- */
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+
 public class DropApp extends GameApplication {
 
     /**
      * Types of entities in this game.
      */
-    public enum Type {
-        DROPLET, BUCKET
+
+    private List<Entity> snakes = new ArrayList<>();
+
+    private int count = 0;
+    private int bigger = 0;
+    private int start = 0;
+    private Point2D lastVector;
+
+    private boolean boost;
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
     protected void initSettings(GameSettings settings) {
         // initialize common game / window settings.
-        settings.setTitle("Drop");
+        settings.setTitle("Snake.io");
         settings.setVersion("1.0");
-        settings.setWidth(480);
+        settings.setWidth(1500);
         settings.setHeight(800);
     }
 
     @Override
     protected void initGame() {
-        spawnBucket();
+        start = 50;
+        spawnSnake();
 
         // creates a timer that runs spawnDroplet() every second
-        run(() -> spawnDroplet(), Duration.seconds(1));
+        run(this::spawnFood, Duration.seconds(1));
 
-        // loop background music located in /resources/assets/music/
-        loopBGM("bgm.mp3");
+    }
+
+    @Override
+    protected void initInput() {
+        getInput().addAction(new UserAction("boost") {
+            @Override
+            protected void onActionBegin() {
+                boost = true;
+            }
+
+            @Override
+            protected void onActionEnd() {
+                boost = false;
+            }
+        }, MouseButton.PRIMARY);
     }
 
     @Override
     protected void initPhysics() {
-        onCollisionBegin(Type.BUCKET, Type.DROPLET, (bucket, droplet) -> {
 
-            // code in this block is called when there is a collision between Type.BUCKET and Type.DROPLET
+        onCollisionBegin(Type.SNAKE, Type.FOOD, (snake, droplet) -> {
+
+            count++;
+
 
             // remove the collided droplet from the game
             droplet.removeFromWorld();
@@ -74,42 +92,101 @@ public class DropApp extends GameApplication {
         });
     }
 
-    @Override
-    protected void onUpdate(double tpf) {
+    private void makeTheSnakeBigger(double x, double y, Point2D rotation) {
 
-        // for each entity of Type.DROPLET translate (move) it down
-        getGameWorld().getEntitiesByType(Type.DROPLET).forEach(droplet -> droplet.translateY(150 * tpf));
-    }
+        var t = texture("snake.png")
+                .subTexture(new Rectangle2D(0, 0, 7, 14))
+                .multiplyColor(Color.GREEN);
 
-    private void spawnBucket() {
-        // build an entity with Type.BUCKET
-        // at the position X = getAppWidth() / 2 and Y = getAppHeight() - 200
-        // with a view "bucket.png", which is an image located in /resources/assets/textures/
-        // also create a bounding box from that view
-        // make the entity collidable
-        // finally, complete building and attach to the game world
-
-        Entity bucket = entityBuilder()
-                .type(Type.BUCKET)
-                .at(getAppWidth() / 2, getAppHeight() - 200)
-                .viewWithBBox("bucket.png")
+        Entity snake = entityBuilder()
+                .type(Type.FOLLOWER)
+                .at(x, y)
+                .viewWithBBox(t)
                 .collidable()
                 .buildAndAttach();
 
-        // bind bucket's X value to mouse X
-        bucket.xProperty().bind(getInput().mouseXWorldProperty());
+        snakes.add(snake);
+        snake.rotateToVector(rotation);
     }
 
-    private void spawnDroplet() {
+    @Override
+    protected void onUpdate(double tpf) {
+
+        double lastX = snakes.get(0).getX();
+        double lastY = snakes.get(0).getY();
+
+        if(boost){
+            snakes.get(0).translate(getInput().getMousePositionWorld().subtract(snakes.get(0).getPosition()).normalize().multiply(1.2));
+        }else {
+            snakes.get(0).translate(getInput().getMousePositionWorld().subtract(snakes.get(0).getPosition()).normalize().multiply(0.8));
+        }
+        Point2D position = snakes.get(0).getPosition();
+        Point2D vectorToMouse = getInput().getMousePositionWorld().subtract(position);
+
+        snakes.get(0).rotateToVector(vectorToMouse);
+
+
+
+        for (int i = 1; i < snakes.size(); i++) {
+            double tempLastX = snakes.get(i).getX();
+            double tempLastY = snakes.get(i).getY();
+            //snakes.get(i).translate(lastX, lastY);
+            snakes.get(i).setX(lastX);
+            snakes.get(i).setY(lastY);
+
+            snakes.get(i).rotateToVector(lastVector);
+
+            lastX = tempLastX;
+            lastY = tempLastY;
+        }
+
+        if (count > 5) {
+            count = 0;
+            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
+            bigger = 20;
+        }else if(bigger > 0){
+            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
+            bigger--;
+        } else if (start> 0) {
+            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
+            start--;
+        }
+
+        lastVector = vectorToMouse;
+    }
+
+
+    private void spawnSnake() {
+        var t = texture("snake.png")
+                .subTexture(new Rectangle2D(0, 0, 7, 14))
+                .multiplyColor(Color.RED);
+
+        Entity snake = entityBuilder()
+                .type(Type.SNAKE)
+                .at(FXGLMath.random(0, getAppWidth()), FXGLMath.random(0, getAppHeight()))
+                .viewWithBBox(t)
+                .collidable()
+                .buildAndAttach();
+
+        snakes.add(snake);
+
+        Point2D position = snake.getPosition();
+        Point2D vectorToMouse = getInput().getMousePositionWorld().subtract(position);
+
+        snake.rotateToVector(vectorToMouse);
+        lastVector = vectorToMouse;
+    }
+
+    private void spawnFood() {
         entityBuilder()
-                .type(Type.DROPLET)
-                .at(FXGLMath.random(0, getAppWidth() - 64), 0)
+                .type(Type.FOOD)
+                .at(FXGLMath.random(0, getAppWidth() - 20), FXGLMath.random(0, getAppHeight() - 20))
                 .viewWithBBox("droplet.png")
                 .collidable()
                 .buildAndAttach();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public enum Type {
+        FOOD, SNAKE, FOLLOWER
     }
 }
---
+++ src/main/java/com/almasb/fxglgames/drop/SnakeEntityInterface.java
@@ -0,0 +1,7 @@
+package com.almasb.fxglgames.drop;
+
+import com.almasb.fxgl.entity.Entity;
+
+public interface SnakeEntityInterface {
+    void onCollisionBegin(Entity other);
+}
