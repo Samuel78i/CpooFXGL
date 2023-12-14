84a8bc375ed7380d10d9e167dcdd0e81b53e78a9
samuel <tardieusamuel@gmail.com>
2023-12-14 18:01:10 +0100

    SnakeComponent

--- src/main/java/com/almasb/fxglgames/drop/SlitherApp.java
+++ src/main/java/com/almasb/fxglgames/drop/SlitherApp.java
@@ -5,6 +5,7 @@ import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxglgames.drop.components.SnakeComponent;
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.input.MouseButton;
@@ -17,21 +18,8 @@ import java.util.List;
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 
-public class DropApp extends GameApplication {
-
-    /**
-     * Types of entities in this game.
-     */
-
+public class SlitherApp extends GameApplication {
     private List<Entity> snakes = new ArrayList<>();
-
-    private int count = 0;
-    private int bigger = 0;
-    private int start = 0;
-    private Point2D lastVector;
-
-    private boolean boost;
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -47,19 +35,18 @@ public class DropApp extends GameApplication {
 
     @Override
     protected void initGame() {
-        start = 50;
         spawnSnake();
         spawnAI();
 
         // creates a timer that runs spawnFood() every second
         run(this::spawnFood, Duration.seconds(1));
-
     }
 
 
     @Override
     protected void initInput() {
-        getInput().addAction(new UserAction("boost") {
+
+/*        getInput().addAction(new UserAction("boost") {
             @Override
             protected void onActionBegin() {
                 boost = true;
@@ -69,86 +56,20 @@ public class DropApp extends GameApplication {
             protected void onActionEnd() {
                 boost = false;
             }
-        }, MouseButton.PRIMARY);
+        }, MouseButton.PRIMARY);*/
     }
 
     @Override
     protected void initPhysics() {
-
         onCollisionBegin(Type.SNAKEHEAD, Type.FOOD, (snake, food) -> {
+            snake.getComponent(SnakeComponent.class).aFoodAsBeenEaten();
 
-            count++;
-
-
-            // remove the collided droplet from the game
+            // remove the collided food from the game
             food.removeFromWorld();
 
             // play a sound effect located in /resources/assets/sounds/
             play("drop.wav");
         });
-    }
-
-    private void makeTheSnakeBigger(double x, double y, Point2D rotation) {
-
-        var t = texture("snake.png")
-                .subTexture(new Rectangle2D(0, 0, 7, 14))
-                .multiplyColor(Color.GREEN);
-
-        Entity snake = entityBuilder()
-                .type(Type.SNAKEBODY)
-                .at(x, y)
-                .viewWithBBox(t)
-                .collidable()
-                .buildAndAttach();
-
-        snakes.add(snake);
-        snake.rotateToVector(rotation);
-    }
-
-    @Override
-    protected void onUpdate(double tpf) {
-
-        double lastX = snakes.get(0).getX();
-        double lastY = snakes.get(0).getY();
-
-        if(boost){
-            snakes.get(0).translate(getInput().getMousePositionWorld().subtract(snakes.get(0).getPosition()).normalize().multiply(1.2));
-        }else {
-            snakes.get(0).translate(getInput().getMousePositionWorld().subtract(snakes.get(0).getPosition()).normalize().multiply(0.8));
-        }
-        Point2D position = snakes.get(0).getPosition();
-        Point2D vectorToMouse = getInput().getMousePositionWorld().subtract(position);
-
-        snakes.get(0).rotateToVector(vectorToMouse);
-
-
-
-        for (int i = 1; i < snakes.size(); i++) {
-            double tempLastX = snakes.get(i).getX();
-            double tempLastY = snakes.get(i).getY();
-            //snakes.get(i).translate(lastX, lastY);
-            snakes.get(i).setX(lastX);
-            snakes.get(i).setY(lastY);
-
-            snakes.get(i).rotateToVector(lastVector);
-
-            lastX = tempLastX;
-            lastY = tempLastY;
-        }
-
-        if (count > 5) {
-            count = 0;
-            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
-            bigger = 20;
-        }else if(bigger > 0){
-            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
-            bigger--;
-        } else if (start> 0) {
-            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
-            start--;
-        }
-
-        lastVector = vectorToMouse;
     }
 
 
@@ -162,15 +83,15 @@ public class DropApp extends GameApplication {
                 .at(FXGLMath.random(0, getAppWidth()), FXGLMath.random(0, getAppHeight()))
                 .viewWithBBox(t)
                 .collidable()
+                .with(new SnakeComponent())
                 .buildAndAttach();
 
         snakes.add(snake);
-
         Point2D position = snake.getPosition();
         Point2D vectorToMouse = getInput().getMousePositionWorld().subtract(position);
 
         snake.rotateToVector(vectorToMouse);
-        lastVector = vectorToMouse;
+        snake.getComponent(SnakeComponent.class).setLastVector(vectorToMouse);
     }
 
     private void spawnFood() {
--- src/main/java/com/almasb/fxglgames/drop/components/SnakeComponent.java
+++ src/main/java/com/almasb/fxglgames/drop/components/SnakeComponent.java
@@ -1,54 +1,112 @@
 package com.almasb.fxglgames.drop.components;
 
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxglgames.drop.Type;
 import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.paint.Color;
 
-import static com.almasb.fxgl.dsl.FXGL.getInput;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class SnakeComponent extends Component {
 
-//    @Override
-//    public void onUpdate(double tpf) {
-//        double lastX = snakes.get(0).getX();
-//        double lastY = snakes.get(0).getY();
-//
-//        if(boost){
-//            snakes.get(0).translate(getInput().getMousePositionWorld().subtract(snakes.get(0).getPosition()).normalize().multiply(1.2));
-//        }else {
-//            snakes.get(0).translate(getInput().getMousePositionWorld().subtract(snakes.get(0).getPosition()).normalize().multiply(0.8));
-//        }
-//        Point2D position = snakes.get(0).getPosition();
-//        Point2D vectorToMouse = getInput().getMousePositionWorld().subtract(position);
-//
-//        snakes.get(0).rotateToVector(vectorToMouse);
-//
-//
-//
-//        for (int i = 1; i < snakes.size(); i++) {
-//            double tempLastX = snakes.get(i).getX();
-//            double tempLastY = snakes.get(i).getY();
-//            //snakes.get(i).translate(lastX, lastY);
-//            snakes.get(i).setX(lastX);
-//            snakes.get(i).setY(lastY);
-//
-//            snakes.get(i).rotateToVector(lastVector);
-//
-//            lastX = tempLastX;
-//            lastY = tempLastY;
-//        }
-//
-//        if (count > 5) {
-//            count = 0;
-//            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
-//            bigger = 20;
-//        }else if(bigger > 0){
-//            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
-//            bigger--;
-//        } else if (start> 0) {
-//            makeTheSnakeBigger(lastX, lastY, vectorToMouse);
-//            start--;
-//        }
-//
-//        lastVector = vectorToMouse;
-//    }
+    List<Entity> bodyParts = new ArrayList<>();
+    int countOfFoodEaten = 0;
+    
+    int countToMakeTheSnakeLonger = 0;
+    int countToMakeTheSnakeLarger = 0;
+    boolean boost = false;
+
+    int start = 50;
+
+    Point2D lastVector;
+
+
+    @Override
+    public void onUpdate(double tpf) {
+        double lastX = this.getEntity().getX();
+        double lastY = this.getEntity().getY();
+
+        if(boost){
+            this.getEntity().translate(getInput().getMousePositionWorld().subtract(
+                    this.getEntity().getPosition()).normalize().multiply(1.2));
+        }else {
+            this.getEntity().translate(getInput().getMousePositionWorld().subtract(
+                    this.getEntity().getPosition()).normalize().multiply(0.8));
+        }
+        Point2D position = this.getEntity().getPosition();
+        Point2D vectorToMouse = getInput().getMousePositionWorld().subtract(position);
+
+        this.getEntity().rotateToVector(vectorToMouse);
+        lastVector = vectorToMouse;
+
+
+        for (Entity bodyPart : bodyParts) {
+            double tempLastX = bodyPart.getX();
+            double tempLastY = bodyPart.getY();
+            //snakes.get(i).translate(lastX, lastY);
+            bodyPart.setX(lastX);
+            bodyPart.setY(lastY);
+
+            //Point2D temp = bodyPart.getRotation();
+
+            bodyPart.rotateToVector(lastVector);
+
+            lastX = tempLastX;
+            lastY = tempLastY;
+        }
+
+        if (countOfFoodEaten > 5) {
+            countOfFoodEaten = 0;
+            makeTheSnakeLonger(lastX, lastY, vectorToMouse);
+            countToMakeTheSnakeLonger = 20;
+        }else if(countToMakeTheSnakeLonger > 0){
+            makeTheSnakeLonger(lastX, lastY, vectorToMouse);
+            countToMakeTheSnakeLonger--;
+            countToMakeTheSnakeLarger+= 5;
+        }else if(countToMakeTheSnakeLarger > 15){
+            makeTheSnakeLarger(lastX, lastY, vectorToMouse);
+            countToMakeTheSnakeLarger--;
+        } else if (start> 0) {
+            makeTheSnakeLonger(lastX, lastY, vectorToMouse);
+            start--;
+        }
+
+    }
+
+    private void makeTheSnakeLarger(double lastX, double lastY, Point2D vectorToMouse) {
+        this.getEntity().setScaleY(2);
+        for (Entity bodyPart : bodyParts) {
+            bodyPart.setScaleY(2);
+        }
+    }
+
+    private void makeTheSnakeLonger(double x, double y, Point2D rotation) {
+        var t = texture("snake.png")
+                .subTexture(new Rectangle2D(0, 0, 7, 14))
+                .multiplyColor(Color.GREEN);
+
+        Entity snake = entityBuilder()
+                .type(Type.SNAKEBODY)
+                .at(x, y)
+                .viewWithBBox(t)
+                .collidable()
+                .buildAndAttach();
+
+        bodyParts.add(snake);
+        snake.rotateToVector(rotation);
+    }
+
+    public void aFoodAsBeenEaten(){
+        countOfFoodEaten++;
+        countToMakeTheSnakeLarger++;
+    }
+
+    public void setLastVector(Point2D lastVector) {
+        this.lastVector = lastVector;
+    }
 }
